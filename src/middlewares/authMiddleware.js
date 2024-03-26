0922cd21a6ab5d58268bf99d4170370e08ee816b
Nguyen Thanh Nam <nvthanhnam121@gmail.com>
2024-03-26 15:56:46 +0700

    middlewares

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,61 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const authAdminMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      status: "ERROR",
+      message: "THE AUTHORIZATION",
+    });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
+    if (err) {
+      return res.status(403).json({
+        status: "ERROR",
+        message: "THE AUTHORIZATION",
+      });
+    }
+    if (user?.isAdmin) {
+      next();
+    } else {
+      return res.status(403).json({
+        status: "ERROR",
+        message: "THE AUTHORIZATION",
+      });
+    }
+  });
+};
+
+const authUserMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      status: "ERROR",
+      message: "THE AUTHORIZATION",
+    });
+  }
+  const userId = req.params.id;
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
+    if (err) {
+      return res.status(403).json({
+        status: "ERROR",
+        message: "THE AUTHORIZATION",
+      });
+    }
+    if (user?.isAdmin || user?.id === userId) {
+      next();
+    } else {
+      return res.status(403).json({
+        status: "ERROR",
+        message: "THE AUTHORIZATION",
+      });
+    }
+  });
+};
+
+module.exports = {
+  authAdminMiddleware,
+  authUserMiddleware,
+};
